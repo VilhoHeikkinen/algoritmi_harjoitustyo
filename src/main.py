2f17f681828b2be275b68916342061b6353af79f
heikkinv <heikkinv@DESKTOP-8GSCL5E>
2024-09-13 19:29:56 +0300

    Lisätty miinaharavaan ruutujen avaaminen

--- src/main.py
+++ src/main.py
@@ -3,4 +3,5 @@ from minesweeper import Minesweeper
 if __name__ == "__main__":
     
     ms = Minesweeper((9,9), 10)
+    ms.open_square(1,2)
     ms.print_grid()
--- src/minesweeper.py
+++ src/minesweeper.py
@@ -8,14 +8,15 @@ class Minesweeper:
         """Luokan konstruktori, joka luo uuden pelin, tai jatkaa peliä annetusta vaiheesta
         
         Args: 
-            koko: Miinaharavan pelikentän koko tuplena (n x m)
+            size: miinaharavan pelikentän koko tuplena (n x m)
+            minecount: miinojen määrä kentällä
         """
 
         self.size = size
         self.rows = size[0]
         self.cols = size[1]
         self.minecount = minecount
-        self.values = self.set_grid()
+        self.values, self.opened = self.set_grid()
         self.set_mines()
         self.count_values()
         
@@ -23,12 +24,14 @@ class Minesweeper:
         """Alustaa kentän ruutujen arvot
         
         Returns:
-            ruutujen_arvot: kaksiulotteinen taulukko, jossa kaikkien miinojen arvoksi asetettu 0
+            values: kaksiulotteinen taulukko, jossa kaikkien miinojen arvoksi asetettu 0
+            opened: kaksiulotteinen taulukko, jossa merkittynä avatut ruudut (0 = ei avattu, 1 = avattu)
         """
         
         values = [[0 for i in range(self.cols)] for j in range(self.rows)]
+        opened = [[0 for i in range(self.cols)] for j in range(self.rows)]
        
-        return values
+        return values, opened
 
     def set_mines(self):
         """Asettaa kentälle miinat
@@ -52,30 +55,84 @@ class Minesweeper:
         """Laskee jokaisen miinattoman ruudun arvon
         """
         
-        for n in range(self.rows):
-            for m in range(self.cols):
-                if self.values[n][m] == -1:
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if self.values[row][col] == -1:
                     continue
                 
                 count = 0
-                if n > 0 and self.values[n-1][m] == -1:
-                    count += 1
-                if n > 0 and m < self.cols-1 and self.values[n-1][m+1] == -1:
-                    count  += 1
-                if m < self.cols-1 and self.values[n][m+1] == -1:
-                    count  += 1
-                if m < self.cols-1 and n < self.rows-1 and self.values[n+1][m+1] == -1:
-                    count  += 1
-                if n < self.rows-1 and self.values[n+1][m] == -1:
-                    count  += 1
-                if n < self.rows-1 and m > 0 and self.values[n+1][m-1] == -1:
-                    count  += 1
-                if m > 0 and self.values[n][m-1] == -1:
-                    count  += 1
-                if m > 0 and n > 0 and self.values[n-1][m-1] == -1:
-                    count  += 1
+                # if n > 0 and self.values[n-1][m] == -1:
+                #     count += 1
+                # if n > 0 and m < self.cols-1 and self.values[n-1][m+1] == -1:
+                #     count  += 1
+                # if m < self.cols-1 and self.values[n][m+1] == -1:
+                #     count  += 1
+                # if m < self.cols-1 and n < self.rows-1 and self.values[n+1][m+1] == -1:
+                #     count  += 1
+                # if n < self.rows-1 and self.values[n+1][m] == -1:
+                #     count  += 1
+                # if n < self.rows-1 and m > 0 and self.values[n+1][m-1] == -1:
+                #     count  += 1
+                # if m > 0 and self.values[n][m-1] == -1:
+                #     count  += 1
+                # if m > 0 and n > 0 and self.values[n-1][m-1] == -1:
+                #     count  += 1
                 
-                self.values[n][m] += count
+                moves = [(-1,0), (-1,1), (0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1)]
+                for move in moves:
+                    new_row = row + move[0]
+                    new_col = col + move[1]
+                    if new_row >= 0 and new_row <= self.rows-1 and new_col >= 0 and new_col <= self.cols-1:
+                        if self.values[new_row][new_col] == -1:
+                            count += 1
+                
+                self.values[row][col] += count
+    
+    def open_square(self, row, col):
+        """Avaa ruutuja laudalta rekursiivisesti. Jos avattu ruutu on arvoltaan 0 (eli vieressä ei pommia),
+           niin avataan myös viereiset ruudut.
+        
+        Args:
+            row: rivi, jolta ruutu avataan
+            col: sarake, jolta ruutu avataan
+        """
+        
+        
+        self.opened[row][col] = 1
+        if self.values[row][col] == 0:
+            # if row > 0 and self.values[row-1][col] != -1:
+            #     if self.opened[row-1][col] != 1:
+            #         self.open_square(row-1, col)
+            # if row > 0 and col < self.cols-1 and self.values[row-1][col+1] != -1:
+            #     if self.opened[row-1][col+1] != 1:
+            #         self.open_square(row-1, col+1)
+            # if col < self.cols-1 and self.values[row][col+1] != -1:
+            #     if self.opened[row][col+1] != 1:
+            #         self.open_square(row, col+1)
+            # if col < self.cols-1 and row < self.rows-1 and self.values[row+1][col+1] != -1:
+            #     if self.opened[row+1][col+1] != 1:
+            #         self.open_square(row+1, col+1)
+            # if row < self.rows-1 and self.values[row+1][col] != -1:
+            #     if self.opened[row+1][col] != 1:
+            #         self.open_square(row+1, col)
+            # if row < self.rows-1 and col > 0 and self.values[row+1][col-1] != -1:
+            #     if self.opened[row+1][col-1] != 1:
+            #         self.open_square(row+1, col-1)
+            # if col > 0 and self.values[row][col-1] != -1:
+            #     if self.opened[row][col-1] != 1:
+            #         self.open_square(row, col-1)
+            # if col > 0 and row > 0 and self.values[row-1][col-1] != -1:
+            #     if self.opened[row-1][col-1] != 1:
+            #         self.open_square(row-1, col-1)
+                    
+            moves = [(-1,0), (0,1), (1,0), (0,-1)]
+            for move in moves:
+                new_row = row + move[0]
+                new_col = col + move[1]
+                if new_row >= 0 and new_row <= self.rows-1 and new_col >= 0 and new_col <= self.cols-1:
+                    if self.opened[new_row][new_col] != 1:
+                        self.open_square(new_row, new_col)
+        
 
     def print_grid(self):
         """Tulostaa pelilaudan
@@ -85,9 +142,12 @@ class Minesweeper:
             print(" " + ((4*self.cols)-1)*"—" + " ")
             line = "|"
             for j in range(self.cols):
-                if self.values[i][j] != -1:
-                    line += " " + str(self.values[i][j]) + " |"
+                if self.opened[i][j] == 1:
+                    if self.values[i][j] != -1:
+                        line += " " + str(self.values[i][j]) + " |"
+                    else:
+                        line += " * |"
                 else:
-                    line += " ? |"
+                    line += "===|"
             print(line)
         print(" " + ((4*self.cols)-1)*"—" + " ")
